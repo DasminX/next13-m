f1f33f4fd9a501053c97680f8f367de7202d0848
DasminX <wojciech.szpila.dev@gmail.com>
2023-09-21 19:50:06 +0200

    tydzien 2 chyba zaliczony

--- src/data/navItems.ts
+++ src/data/navItems.ts
@@ -3,5 +3,4 @@ import { type NavElementType } from "@/types";
 export const navItems: NavElementType[] = [
 	{ text: "Home", path: "/" },
 	{ text: "Products", path: "/products/1" },
-	{ text: "Contact", path: "/contact" },
 ];
